43f892be5ddfa4b6f400accf6f5eed21cb2deaf4
FabianaMendes <fabianamendesmg@yahoo.com.br>
2022-04-27 10:55:51 -0300

    Atualiza página de experiências

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,12 +1,12 @@
 import './style.css';
 
-export default function Card(props) {
+export default function Card({ position, company, time, description }) {
     return(
         <div className="card-container">
-            <h3>{props.position}</h3>
-            <h4>{props.company}</h4>
-            <h5>{props.time}</h5>
-            <p>{props.description}</p>
+            <h3>{position}</h3>
+            <h4>{company}</h4>
+            <h5>{time}</h5>
+            <p>{description}</p>
         </div>
     );
 }
