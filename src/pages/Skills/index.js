f02a8bde32bf454c8f6d2cb6060b0797f13690c5
FabianaMendes <fabianamendesmg@yahoo.com.br>
2022-04-27 14:23:50 -0300

    Atualiza pagina skills, adiciona competencias e altera estilos

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -3,173 +3,189 @@ import SkillBar from '../../components/SkillBar';
 import './style.css';
 
 export default function Skills() {
-    return (
-        <>
-            <div className="container">
-                <h2>Courses</h2>
-                <div className="line"></div>
-                    <div className="courses-container">
-                        <Card
-                            position="Formação React.js"
-                            company="Alura"
-                            time="100h"
-                            description="Mar. 2021"
-                        />
-                        <Card
-                            position="Java Spring Boot + React.js + React Native"
-                            company="DevSuperior"
-                            time="16h"
-                            description="Jan. 2021"
-                        />
-                        <Card
-                            position="HTML5 e CSS3 Completo"
-                            company="Udemy"
-                            time="6h"
-                            description="Dez. 2020"
-                        />
-                        <Card
-                            position="Javascript ES6 Essencial"
-                            company="Digital Innovation One"
-                            time=""
-                            description="Dez. 2020"
-                        />
-                        <Card
-                            position="Java - Programação Orientada a Objetos"
-                            company="Udemy"
-                            time="50h"
-                            description="Out. 2020"
-                        />
-                        <Card
-                            position="Java - Lógica de Programação e algoritmos"
-                            company="Udemy"
-                            time="8h"
-                            description="Mar. 2020"
-                        />
-                        <Card
-                            position="Gestão Ágil om Scrum"
-                            company="Udemy"
-                            time="6.5h"
-                            description="Jan. 2020"
-                        />
-                        <Card
-                            position="Empreendedorismo, Negócios e Startups"
-                            company="Fast MBA"
-                            time="7h"
-                            description="Jan. 2020"
-                        />
-                        <Card
-                            position="Liderança e Gestão de Pessoas"
-                            company="Fast MBA Lead"
-                            time="7h"
-                            description="Jan. 2020"
-                        />
-                    </div>
-                <h2>My Skills</h2>
-                <div className="line"></div>
-                <h3>Hard Skills</h3>
-                    <div className="skills">
-                        <SkillBar 
-                            name="HTML"
-                            size="70"
-                        />
-                        <SkillBar 
-                            name="CSS"
-                            size="70"
-                        />
-                        <SkillBar 
-                            name="Javascript"
-                            size="60"
-                        />
-                        <SkillBar 
-                            name="React"
-                            size="60"
-                        />
-                        <SkillBar 
-                            name="Figma"
-                            size="60"
-                        />
-                        <SkillBar 
-                            name="Scrum"
-                            size="50"
-                        />
-                        <SkillBar 
-                            name="Git/Github/Gitlab"
-                            size="50"
-                        />
-                        <SkillBar 
-                            name="Java"
-                            size="30"
-                        />
-                        <SkillBar 
-                            name="Typescript"
-                            size="20"
-                        />
-                        <SkillBar 
-                            name="Bootstrap"
-                            size="20"
-                        />
-                        <SkillBar 
-                            name="Material UI"
-                            size="10"
-                        />
-                    </div>
-                    <br/>
-                <h3>Soft Skills</h3>
-                    <div className="skills">
-                        <SkillBar 
-                            name="Flexibilidade"
-                            size="80"
-                        />
-                        <SkillBar 
-                            name="Resolução de problemas"
-                            size="80"
-                        />
-                        <SkillBar 
-                            name="Ética"
-                            size="80"
-                        />
-                        <SkillBar 
-                            name="Empatia"
-                            size="80"
-                        />
-                        <SkillBar 
-                            name="Comunicação oral e escrita"
-                            size="80"
-                        />
-                        <SkillBar 
-                            name="Foco"
-                            size="80"
-                        />
-                        <SkillBar 
-                            name="Resiliência"
-                            size="80"
-                        />
-                        <SkillBar 
-                            name="Colaboração"
-                            size="70"
-                        />
-                        <SkillBar 
-                            name="Liderança"
-                            size="70"
-                        />
-                        <SkillBar 
-                            name="Organização"
-                            size="70"
-                        />
-                        <SkillBar 
-                            name="Autonomia"
-                            size="70"
-                        />
-                        <SkillBar 
-                            name="Negociação"
-                            size="60"
-                        />
-                        <SkillBar 
-                            name="Criatividade"
-                            size="50"
-                        />
-                    </div>
-            </div>
-        </>
-    );
+  return (
+    <div className="container">
+      <h2>Courses</h2>
+      <div className="line"></div>
+      <ul className="courses-container">
+        <li>
+          <h4>DIO Campus Expert</h4>
+          <h5>Digital Innovation One</h5>
+          <p><span>Carga Horária: </span>20h</p>
+          <p><span>Conclusão: </span>Março/2022</p>
+        </li>
+        <li>
+          <h4>Curso Vue.js do zero ao avançado na prática</h4>
+          <h5>Udemy</h5>
+          <p><span>Carga Horária: </span>12h</p>
+          <p><span>Conclusão: </span>Novembro/2021</p>
+        </li>
+        <li>
+          <h4>React.js + Typescript - Dashboard Completo</h4>
+          <h5>Udemy</h5>
+          <p><span>Carga Horária: </span>16.5h</p>
+          <p><span>Conclusão: </span>Julho/2021</p>
+        </li>
+        <li>
+          <h4>Formação React.js</h4>
+          <h5>Alura</h5>
+          <p><span>Carga Horária: </span>100h</p>
+          <p><span>Conclusão: </span>Março/2021</p>
+        </li>
+        <li>
+          <h4>Java Spring Boot + React.js + React Native</h4>
+          <h5>DevSuperior</h5>
+          <p><span>Carga Horária: </span>16h</p>
+          <p><span>Conclusão: </span>Janeiro/2021</p>
+        </li>
+        <li>
+          <h4>HTML5 e CSS3 Completo</h4>
+          <h5>Udemy</h5>
+          <p><span>Carga Horária: </span>6h</p>
+          <p><span>Conclusão: </span>Dezembro/2020</p>
+        </li>
+        <li>
+          <h4>Javascript ES6 Essencial</h4>
+          <h5>Digital Innovation One</h5>
+          <p><span>Carga Horária: </span>-</p>
+          <p><span>Conclusão: </span>Dezembro/2020</p>
+        </li>
+        <li>
+          <h4>Java - Programação Orientada a Objetos</h4>
+          <h5>Udemy</h5>
+          <p><span>Carga Horária: </span>50h</p>
+          <p><span>Conclusão: </span>Outubro/2020</p>
+        </li>
+        <li>
+          <h4>Java - Lógica de Programação e algoritmos</h4>
+          <h5>Udemy</h5>
+          <p><span>Carga Horária: </span>8h</p>
+          <p><span>Conclusão: </span>Março/2020</p>
+        </li>
+        <li>
+          <h4>Gestão Ágil com Scrum</h4>
+          <h5>Udemy</h5>
+          <p><span>Carga Horária: </span>6.5h</p>
+          <p><span>Conclusão: </span>Janeiro/2020</p>
+        </li>
+        <li>
+          <h4>Empreendedorismo, Negócios e Startups</h4>
+          <h5>Fast MBA</h5>
+          <p><span>Carga Horária: </span>7h</p>
+          <p><span>Conclusão: </span>Janeiro/2020</p>
+        </li>
+        <li>
+          <h4>Liderança e Gestão de Pessoas</h4>
+          <h5>Fast MBA Lead</h5>
+          <p><span>Carga Horária: </span>7h</p>
+          <p><span>Conclusão: </span>Janeiro/2020</p>
+        </li>
+        <li>
+          <h4>Modelagem de Dados</h4>
+          <h5>Fundação Bradesco</h5>
+          <p><span>Carga Horária: </span>12h</p>
+          <p><span>Conclusão: </span>Fevereiro/2019</p>
+        </li>
+        <li>
+          <h4>Empreendedorismo e Inovação</h4>
+          <h5>Fundação Bradesco</h5>
+          <p><span>Carga Horária: </span>12h</p>
+          <p><span>Conclusão: </span>Fevereiro/2019</p>
+        </li>
+      </ul>
+      <h2>My Skills</h2>
+      <div className="line"></div>
+      <h3>Hard Skills</h3>
+      <div className="skills">
+        <SkillBar
+          name="React Native"
+          size="80"
+        />
+        <SkillBar
+          name="React"
+          size="80"
+        />
+        <SkillBar
+          name="HTML"
+          size="80"
+        />
+        <SkillBar
+          name="CSS"
+          size="80"
+        />
+        <SkillBar
+          name="Typescript"
+          size="80"
+        />
+        <SkillBar
+          name="Scrum"
+          size="80"
+        />
+        <SkillBar
+          name="Javascript"
+          size="70"
+        />
+        <SkillBar
+          name="Git/Github/Gitlab"
+          size="70"
+        />
+        <SkillBar
+          name="Figma"
+          size="60"
+        />
+        <SkillBar
+          name="Design de Interfaces"
+          size="30"
+        />
+        <SkillBar
+          name="Next.js"
+          size="30"
+        />
+        <SkillBar
+          name="Vue.js"
+          size="20"
+        />
+        <SkillBar
+          name="App Center"
+          size="20"
+        />
+        <SkillBar
+          name="AWS S3"
+          size="10"
+        />
+        <SkillBar
+          name="Java"
+          size="10"
+        />
+        <SkillBar
+          name="Bootstrap"
+          size="10"
+        />
+        <SkillBar
+          name="Material UI"
+          size="10"
+        />
+      </div>
+      <br />
+      <h3>Soft Skills</h3>
+      <ul className="skills">
+        <li >Flexibilidade</li>
+        <li>Resolução de problemas</li>
+        <li>Ética</li>
+        <li>Empatia</li>
+        <li>Comunicação oral</li>
+        <li>Comunicação escrita</li>
+        <li>Curiosidade</li>
+        <li>Foco</li>
+        <li>Resiliência</li>
+        <li>Colaboração</li>
+        <li>Liderança</li>
+        <li>Organização</li>
+        <li>Autonomia</li>
+        <li>Auto gestão</li>
+        <li>Negociação</li>
+        <li>Criatividade</li>
+      </ul>
+    </div>
+  );
 }
